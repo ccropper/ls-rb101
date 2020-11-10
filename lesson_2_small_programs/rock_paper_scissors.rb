6d18a4e91455ba8467e34dcfb6b7ff9b27f3a5bd
Caiqian Cropper <ccropper@concertgenetics.com>
2020-11-09 20:14:46 -0600

    RPS bonus: +lizard +spock, shorthand feature, and tournament mode

--- lesson_2_small_programs/rock_paper_scissors.rb
+++ lesson_2_small_programs/rock_paper_scissors.rb
@@ -1,30 +1,57 @@
-VALID_CHOICES = %w(rock paper scissors)
+VALID_CHOICES = %w(rock paper scissors lizard spock)
+
+RPS_LOOKUP = {
+  rock: %w(scissors spock),
+  paper: %w(rock lizard),
+  scissors: %w(lizard paper),
+  spock: %w(spock scissors),
+  lizard: %w(paper spock)
+}
+
+SHORTHAND_LOOKUP = {
+  r: 'rock',
+  p: 'paper',
+  sc: 'scissors',
+  sp: 'spock',
+  l: 'lizard'
+}
 
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
+def shorthand_to_full_choice(shorthand)
+  SHORTHAND_LOOKUP[shorthand.to_sym]
+end
+
 def win?(first, second)
-  (first == 'rock' && second == 'scissors') ||
-  (first == 'paper' && second == 'rock') ||
-  (first == 'scissors' && second == 'paper') ||
+  RPS_LOOKUP[first.to_sym].include?(second)
 end
 
 def display_results(player, computer)
   if win?(player, computer)
-    prompt("You won!")
+    prompt("You won this round!")
   elsif win?(computer, player)
-    prompt("You lost!")
+    prompt("You lost this round!")
   end
 end
+
+player_wins = 0
+computer_wins = 0
+
+prompt("Welcome to #{VALID_CHOICES.join(', ')}. "\
+       "First to 5 wins is the winner!")
 
 loop do
   choice = ''
   loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    prompt("Choose one: r, p, sc, l, sp")
     choice = Kernel.gets().chomp()
 
     if VALID_CHOICES.include?(choice)
+      break
+    elsif SHORTHAND_LOOKUP.include?(choice.to_sym)
+      choice = shorthand_to_full_choice(choice)
       break
     else
       prompt("That's not a valid choice.")
@@ -33,13 +60,26 @@ loop do
 
   computer_choice = VALID_CHOICES.sample
 
-  Kernel.puts("You chose #{choice}; Computer chose #{computer_choice}.")
+  Kernel.puts("You chose #{choice}; "\
+              "Computer chose #{computer_choice}.")
 
   display_results(choice, computer_choice)
 
-  prompt("Do you want to play again?")
-  answer = Kernel.gets().chomp()
-  break unless answer.downcase().start_with?('y')
+  if win?(choice, computer_choice)
+    player_wins += 1
+  elsif win?(computer_choice, choice)
+    computer_wins += 1
+  end
+
+  if player_wins == 5
+    prompt("YOU ARE THE GRAND TOURNAMENT WINNER!!!")
+    break
+  elsif computer_wins == 5
+    prompt("You lost the tournament...")
+    break
+  end
 end
 
+prompt("The final score was #{player_wins} wins for you; "\
+  "#{computer_wins} wins for the computer.")
 prompt("Thank you for playing. Goodbye!")
