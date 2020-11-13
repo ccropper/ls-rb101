5ef59ad72ebaa0119370055029edc0e6dfb0fe5b
Caiqian Cropper <ccropper@concertgenetics.com>
2020-11-13 08:53:02 -0600

    lesson 3 practice problems: medium 1

---
+++ lesson_3_practice_problems/ascii_art.rb
@@ -0,0 +1,6 @@
+# READ "The Flintstones Rock!"
+# loop through 10 times
+#   add ' ' to the front of "The Flintstones Rock"
+#   PRINT as output
+
+10.times {|num| puts (" " * num) + "The Flintstones Rock!\n"}
