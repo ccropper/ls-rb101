2dd6cc83b4794654e6ecc3dd893029999a0a7875
Caiqian Cropper <ccropper@concertgenetics.com>
2021-01-02 21:33:46 -0600

    tictactoe bonus feature 2. scorekeeping and 6. improve game loop

--- lesson_6_slightly_larger_programs/tictactoe.rb
+++ lesson_6_slightly_larger_programs/tictactoe.rb
@@ -1,4 +1,6 @@
 require 'pry'
+require 'io/console'
+
 
 WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +
                 [[1, 5, 9], [3, 5, 7], [1, 4, 7]] +
@@ -8,6 +10,9 @@ INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
 
+SCORE = { "Player" => 0,
+          "Computer" => 0 }
+
 def prompt(msg)
   puts "=> #{msg}"
 end
@@ -16,14 +21,22 @@ def joinor(arr,
            delimiter = ',',
            type = 'or')
   joined = ''
-  arr = arr.map { |n| n.to_s}
+  arr = arr.map(&:to_s)
   if arr.length < 3
     joined << arr.join(' ' + type + ' ')
   else
-    joined << arr[0,arr.length-1].join(delimiter + ' ')
+    joined << arr[0, arr.length - 1].join(delimiter + ' ')
     joined << "#{delimiter} #{type} #{arr.last}"
   end
   joined
+end
+
+def alternate_player(current_player)
+  if current_player == 'Player'
+    'Computer'
+  else 
+    'Player'
+  end
 end
 
 # rubocop: disable Metrics/AbcSize
@@ -57,16 +70,40 @@ def empty_squares(brd)
   brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
-def player_places_piece!(brd)
-  square = ''
-  loop do
-    prompt "Choose a square (#{joinor(empty_squares(brd))}): "
-    square = gets.chomp.to_i
-    break if empty_squares(brd).include?(square)
-    prompt "Sorry, that's not a valid choice"
+def place_piece!(brd, current_player)
+  square = nil
+  if current_player == 'Player'
+    loop do
+      prompt "Choose a square (#{joinor(empty_squares(brd))}): "
+      square = STDIN.getch.to_i
+      break if empty_squares(brd).include?(square)
+      prompt "Sorry, that's not a valid choice"
+    end
+    brd[square] = PLAYER_MARKER
+  else
+    loop do
+      square = find_winning_square(brd, COMPUTER_MARKER)
+      break if square
+      square = find_winning_square(brd, PLAYER_MARKER)
+      break if square
+      square = empty_squares(brd).sample
+      break
+    end
+    brd[square] = COMPUTER_MARKER
   end
-  brd[square] = PLAYER_MARKER
 end
+  
+
+# def player_places_piece!(brd)
+#   square = nil
+#   loop do
+#     prompt "Choose a square (#{joinor(empty_squares(brd))}): "
+#     square = STDIN.getch.to_i
+#     break if empty_squares(brd).include?(square)
+#     prompt "Sorry, that's not a valid choice"
+#   end
+#   brd[square] = PLAYER_MARKER
+# end
 
 def find_winning_square(brd, marker)
   square = nil
@@ -83,18 +120,18 @@ def find_winning_square(brd, marker)
   square
 end
 
-def computer_places_piece!(brd)
-  square = nil
-  loop do
-    square = find_winning_square(brd, COMPUTER_MARKER)
-    break if square
-    square = find_winning_square(brd, PLAYER_MARKER)
-    break if square
-    square = empty_squares(brd).sample
-    break
-  end
-  brd[square] = COMPUTER_MARKER
-end
+# def computer_places_piece!(brd)
+#   square = nil
+#   loop do
+#     square = find_winning_square(brd, COMPUTER_MARKER)
+#     break if square
+#     square = find_winning_square(brd, PLAYER_MARKER)
+#     break if square
+#     square = empty_squares(brd).sample
+#     break
+#   end
+#   brd[square] = COMPUTER_MARKER
+# end
 
 def board_full?(brd)
   empty_squares(brd).empty?
@@ -118,26 +155,34 @@ end
 
 loop do
   board = initialize_board
+  current_player = 'Player'
 
   loop do
     display_board(board)
-    player_places_piece!(board)
+    place_piece!(board, current_player)
+    # player_places_piece!(board)
     break if someone_won?(board) || board_full?(board)
-    computer_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
+    # computer_places_piece!(board)
+    # break if someone_won?(board) || board_full?(board)
+    current_player = alternate_player(current_player)
   end
 
   display_board(board)
 
   if someone_won?(board)
-    prompt "#{detect_winner(board)} won!"
+    prompt "+1 point for #{detect_winner(board)}!"
+    SCORE[detect_winner(board)] += 1
   else
     prompt "It's a tie!"
   end
 
-  prompt "Play again? (y or n)"
-  answer = gets.chomp
-  break unless answer.downcase.start_with?('y')
+  prompt "You have #{SCORE['Player']} points."
+  prompt "Computer has #{SCORE['Computer']} points."
+  break if SCORE.values.any? { |n| n >= 5 }
+  prompt "Press any key to continue."
+  continue = STDIN.getch
+  system 'clear' if continue
 end
 
+prompt "#{SCORE.max_by { |_, wins| wins }[0]} wins a the game with first to 5 points!"
 prompt "Thanks for playing Tic Tac Toe. Goodbye!"
