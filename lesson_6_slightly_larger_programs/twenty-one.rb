0999948aa7820499cfae08c55a6d5267757f4089
Caiqian Cropper <ccropper@concertgenetics.com>
2021-01-10 13:45:49 -0600

    bonus features

--- lesson_6_slightly_larger_programs/twenty-one.rb
+++ lesson_6_slightly_larger_programs/twenty-one.rb
@@ -1,3 +1,12 @@
+require 'io/console'
+
+GOAL_SCORE = 21
+
+GOAL_WINS = 5
+
+SCORE = { "Player" => 0,
+          "Dealer" => 0 }
+
 FULL_DECK = [['H', 'A'], ['H', '2'], ['H', '3'], ['H', '4'], ['H', '5'],
              ['H', '6'], ['H', '7'], ['H', '8'], ['H', '9'], ['H', '10'],
              ['H', 'J'], ['H', 'Q'], ['H', 'K'], ['D', 'A'], ['D', '2'],
@@ -28,9 +37,15 @@ def prompt(msg)
   puts "=> #{msg}"
 end
 
+def press_key_to_continue
+  prompt "Press any key to continue."
+  continue = STDIN.getch
+  system 'clear' if continue
+end
+
 def display_welcome
   system 'clear'
-  prompt "Welcome to Twenty-One."
+  prompt "Welcome to #{GOAL_SCORE}."
   puts "----------------------------------"
 end
 
@@ -51,7 +66,7 @@ end
 def display_hand(hand, dealer=false, hide_card=false)
   if dealer && hide_card
     cards_to_display = joinor(hand[1..hand.length].map { |n| n[1] })
-    prompt("Dealer's hand: #{cards_to_display} and an unknown card.")
+    prompt("Dealer's hand: #{cards_to_display} plus an unknown card.")
   elsif dealer
     cards_to_display = joinor(hand.map { |n| n[1] })
     prompt("Dealer hand: #{cards_to_display}.")
@@ -66,10 +81,10 @@ end
 def calculate_hand_total(hand)
   hand_values = hand.map { |card| CARD_VALUES[card[1]][0] }
   hand_total = hand_values.sum
-  if hand_total > 21
+  if hand_total > GOAL_SCORE
     aces = hand.count { |_, value| value == 'A' }
     aces.times do
-      if hand_total > 21
+      if hand_total > GOAL_SCORE
         hand_total -= 10
       end
     end
@@ -78,32 +93,32 @@ def calculate_hand_total(hand)
 end
 
 def busted?(hand)
-  calculate_hand_total(hand) > 21
+  calculate_hand_total(hand) > GOAL_SCORE
 end
-  
-def player_turn(hand, deck)
+
+def player_turn!(hand, deck)
   loop do
     prompt("Do you want to hit or stay?")
     answer = gets.chomp.downcase
-    break if answer == "stay"
-    if answer == 'hit'
-      hand += deck.shift(1)
+    break if answer.start_with?('s')
+    if answer.start_with?('h')
+      hand.append(deck.shift(1)[0])
       display_hand(hand)
-    end
-    if busted?(hand)
-      prompt("You bust!")
-      break
+      if busted?(hand)
+        prompt("You bust!")
+        break
+      end
     end
   end
   hand
 end
 
-def dealer_turn(hand, deck)
+def dealer_turn!(hand, deck)
   loop do
     hand_total = calculate_hand_total(hand)
-    break if hand_total >= 17
+    break if hand_total >= GOAL_SCORE - 4
     prompt("Dealer hits...")
-    hand += deck.shift(1)
+    hand.append(deck.shift(1)[0])
     display_hand(hand, dealer=true, hide_card=true)
     if busted?(hand)
       prompt("Dealer busts!")
@@ -114,7 +129,6 @@ def dealer_turn(hand, deck)
 end
 
 def reveal_hands(player_hand, dealer_hand)
-  puts "----------------------------------"
   prompt("Revealing hands...")
   display_hand(dealer_hand, dealer=true)
   display_hand(player_hand)
@@ -123,9 +137,9 @@ end
 def who_won?(player_hand, dealer_hand)
   player_score = calculate_hand_total(player_hand)
   dealer_score = calculate_hand_total(dealer_hand)
-  if player_score > 21
+  if busted?(player_hand)
     'Dealer'
-  elsif dealer_score > 21
+  elsif busted?(dealer_hand)
     'Player'
   elsif player_score > dealer_score
     'Player'
@@ -134,7 +148,7 @@ def who_won?(player_hand, dealer_hand)
   end
 end
 
-def declare_winner(winner)
+def display_winner(winner)
   if winner == 'Player'
     prompt("You win!")
   elsif winner == 'Dealer'
@@ -144,6 +158,17 @@ def declare_winner(winner)
   end
 end
 
+def update_score(winner)
+  if winner
+    SCORE[winner] += 1
+  end
+end
+
+def display_current_scores
+  prompt "You have #{SCORE['Player']} points."
+  prompt "Dealer has #{SCORE['Dealer']} points."
+end
+
 def play_again?
   puts "----------------------------------"
   prompt "Do you want to play again? (y or n)"
@@ -151,30 +176,54 @@ def play_again?
   answer.downcase.start_with?('y')
 end
 
-display_welcome
+def someone_grand_winner?
+  SCORE.values.any? { |n| n >= GOAL_WINS }
+end
+
+def display_grand_winner
+  prompt "#{SCORE.max_by { |_, wins| wins }[0]} wins best out of 5!"
+end
+
+def display_goodbye
+  prompt "Thanks for playing #{GOAL_SCORE}. Goodbye!"
+end
+
+def reset_scores
+  SCORE["Player"] = 0
+  SCORE["Dealer"] = 0
+end
+
 
 loop do
+  display_welcome
+  reset_scores
   current_deck = FULL_DECK.shuffle
-
-  # 2. Deal cards to player and dealer
-
-  player_hand = current_deck.shift(2)
-  dealer_hand = current_deck.shift(2)
-
-  display_hand(dealer_hand, dealer=true, hide_card=true)
-  display_hand(player_hand)
-
   loop do
-    player_hand = player_turn(player_hand, current_deck)
-    break if busted?(player_hand)
+    player_hand = current_deck.shift(2)
+    dealer_hand = current_deck.shift(2)
 
-    dealer_hand = dealer_turn(dealer_hand, current_deck)
-    break
+    display_hand(dealer_hand, dealer=true, hide_card=true)
+    display_hand(player_hand)
+
+    loop do
+      player_turn!(player_hand, current_deck)
+      break if busted?(player_hand)
+
+      dealer_turn!(dealer_hand, current_deck)
+      break
+    end
+
+    reveal_hands(player_hand, dealer_hand)
+    winner = who_won?(player_hand, dealer_hand)
+    display_winner(winner)
+    update_score(winner)
+
+    break if someone_grand_winner?
+    display_current_scores
+    press_key_to_continue
   end
 
-  reveal_hands(player_hand, dealer_hand)
-  winner = who_won?(player_hand, dealer_hand)
-  declare_winner(winner)
-
+  display_grand_winner
   break unless play_again?
 end
+display_goodbye
